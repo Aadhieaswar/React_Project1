1ef0910a56ec2ebe193d53531db4f797ac5c2ed4
Aadhi <64110005+Aadhieaswar@users.noreply.github.com>
2020-07-19 15:25:17 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
-// import items from other files below
+// importing components from other files
 import Timer from './timer.js'
 
 export default class App extends React.Component {
   render() {
       return (
         <View style={styles.container}>
-            <Text style={styles.text}>Work Timer</Text>
+            <Text style={[styles.text, styles.boxShadow]}>Work Timer</Text>
             <Timer/>
         </View>
     );
@@ -16,19 +16,28 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
     text: {
         fontSize: 50,
         color: '#1d2124',
-        fontWeight: 'bold',
         borderWidth: 2,
         borderColor: '#1d2124',
         padding: 10,
-        borderRadius: 5,
+        fontWeight: 'bold',
+        backgroundColor: 'white',
     },
-});
+    boxShadow: {
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.50, //0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    }
+  });
